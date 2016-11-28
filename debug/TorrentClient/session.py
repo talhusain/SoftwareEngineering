debc5af89fa915b2ba0b93a84b18f7b02e83ee10
Adam Furbee <adam.furbee@gmail.com>
2016-11-28 14:13:32 -0600

    adjusted socket settings

--- debug/TorrentClient/session.py
+++ debug/TorrentClient/session.py
@@ -80,7 +80,7 @@ class Session(threading.Thread):
         handshake = self.generate_handshake()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.settimeout(10)
+        self.socket.setblocking(True)
         try:
             self.socket.connect(self.peer)
         except Exception:
